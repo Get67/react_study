ec4e49ac25a51cddb217ebe5d4779df1020b7f9d
Get67 <a1099668033@qq.com>
2023-03-13 15:20:52 +0800

    077-2023年3月13日15:20:49

--- react_staging/src/index.js
+++ react_staging/src/index.js
@@ -5,12 +5,16 @@ import ReactDOM from 'react-dom'
 //引入App
 import App from './App'
 
-
+import { BrowserRouter } from 'react-router-dom'
 
 
 
 //渲染App到页面
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    , document.getElementById('root'))
 
 
 
